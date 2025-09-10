2428e2995db2241dfa4fd279eb7d6f0956627bce
Pranith Bitling <140081653+Pranith15@users.noreply.github.com>
2025-09-10 14:14:07 +0530

    Add files via upload

---
+++ IdentifiersDemo.java
@@ -0,0 +1,23 @@
+package com.tns.identifiers;
+
+public class IdentifiersDemo {
+	public static void main(String[] args) {
+		//Invalid identifiers
+		/*int while or num 2 = 24; 1.we cannot use reserved word or keyword
+		 *   as identifier. 2.we cannot use space after identifier
+		System.out.println(while);
+		*/
+		
+		//valid Identifiers
+		int number1=28;
+		System.out.println("print the number1:"+number1);
+		int $number2=27;
+		System.out.println("print the number2:"+$number2);
+		int number_3=62;
+		System.out.println("print the number3:"+number_3);
+		String StudentName="Pranith";
+		System.out.println("print the name:"+StudentName);
+		
+	}
+
+}
